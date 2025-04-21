a36a491295f7a20279036535f43fefa4ff05ecc3
dmnoky <48329889+dmnoky@users.noreply.github.com>
2025-04-21 23:47:31 +0500

    Delete units/unit_8 directory

--- units/unit_8/unit_8-bad/src/main/resources/db/changelog/postgres/rollback/unit8_create-transport_client-rollback.sql
+++
@@ -1,5 +0,0 @@
-DROP INDEX IF EXISTS unit8.index_client_transport;
-DROP TABLE IF EXISTS unit8.client_transport;
-DROP INDEX IF EXISTS unit8.index_transport_gos_reg_num;
-DROP INDEX IF EXISTS unit8.index_transport_model_id;
-DROP TABLE IF EXISTS unit8.transport;
--- units/unit_8/unit_8/src/main/resources/db/changelog/postgres/rollback/unit8_create-client-rollback.sql
+++
@@ -1,2 +0,0 @@
-DROP INDEX IF EXISTS unit8.index_client_delete_at;
-DROP TABLE IF EXISTS unit8.client;
